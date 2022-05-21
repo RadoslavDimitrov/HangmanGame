cd888043046496ca6313ea82219b1d6119881cc4
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2022-05-21 15:13:38 +0300

    Add start new game and word update

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -8,8 +8,21 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            StartGame();
+            bool result = StartGame();
 
+            while (result == true)
+            {
+                result = StartGame();
+            }
+
+            
+            
+            //TODO start new game
+        }
+
+        private static bool StartGame()
+        {
+            Console.Clear();
             const int maxWrongAnswers = 5;
             int wrongAnswers = 0;
 
@@ -18,8 +31,8 @@ namespace Hangman
             string word = "Hello";
             string wordDescription = "Something you say to every person you know and meet";
 
-            List<char> wordResult = FillResultList(word); 
-            
+            List<char> wordResult = FillResultList(word);
+
 
             Console.WriteLine(DrowBegginingScreen());
             Console.WriteLine();
@@ -27,27 +40,41 @@ namespace Hangman
             Console.WriteLine(wordDescription);
             Console.WriteLine();
             Console.WriteLine(GenerateEmptyWord(word.Length, word, wordResult));
-            
+
 
             while (true)
             {
                 Console.WriteLine("Type a letter that you think, the word has it");
-                char answer = char.Parse(Console.ReadLine());
+                char answer = char.Parse(Console.ReadLine().Substring(0,1));
+
+                //IndexOf not found = -1;
+                //Contains("string", startIndex)
+
 
                 if (word.ToLower().Contains(answer))
                 {
-                    Console.WriteLine(UpdateScreen(wrongAnswers));
-                    //we have a match
+                    int index = word.ToLower().IndexOf(answer);
+                    if(wordResult[index] != answer)
+                    {
+                        wordResult[index] = answer;
+                    }
+
+                    //TODO case double letter
+                    //TODO alreagy have this letter
                     //TODO update word showing
+
+                    Console.WriteLine(UpdateScreen(wrongAnswers, word, wordDescription, wordResult));
+
+
                 }
                 else
                 {
                     //we update screen with one more wrong answers
                     wrongAnswers++;
-                    Console.WriteLine(UpdateScreen(wrongAnswers));
+                    Console.WriteLine(UpdateScreen(wrongAnswers, word, wordDescription, wordResult));
                 }
 
-                if(wrongAnswers == maxWrongAnswers)
+                if (wrongAnswers == maxWrongAnswers)
                 {
                     break;
                 }
@@ -56,14 +83,12 @@ namespace Hangman
             bool newGame = LoseScreen();
             if (newGame)
             {
-                StartGame();
+                return true;
             }
-            //TODO start new game
-        }
-
-        private static void StartGame()
-        {
-            throw new NotImplementedException();
+            else
+            {
+                return false;
+            }
         }
 
         private static bool LoseScreen()
@@ -72,9 +97,9 @@ namespace Hangman
             Console.WriteLine("You have been hanged!");
             Console.WriteLine();
             Console.WriteLine("Do you wanna play again? Type Y for \"Yes\" or N for \"No\"");
-            char answer = char.Parse(Console.ReadLine());
+            string answer = Console.ReadLine().ToLower();
 
-            if(answer == 'Y')
+            if(answer[0] == 'y')
             {
                 return true;
             }
@@ -85,9 +110,17 @@ namespace Hangman
 
         }
 
-        private static string UpdateScreen(int wrongAnswers)
+        private static string UpdateScreen(int wrongAnswers, string word, string wordDescription, List<char> wordResult)
         {
             StringBuilder result = new StringBuilder();
+
+            result.AppendLine(DrowBegginingScreen());
+            result.AppendLine();
+            result.AppendLine();
+            result.AppendLine(wordDescription);
+            result.AppendLine();
+            result.AppendLine(UpdateWord(word.Length, word, wordResult));
+
             result.AppendLine("..........");
             result.AppendLine("----------");
             result.AppendLine(".|...._...");
@@ -134,13 +167,25 @@ namespace Hangman
             return result.ToString();
         }
 
+        private static string UpdateWord(int length, string word, List<char> wordResult)
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append($"{wordResult[i]} ");
+            }
+
+            return sb.ToString();
+        }
+
         private static List<char> FillResultList(string word)
         {
             var result = new List<char>();
 
             foreach (var ch in word)
             {
-                result.Add('0');
+                result.Add('_');
             }
 
             return result;
